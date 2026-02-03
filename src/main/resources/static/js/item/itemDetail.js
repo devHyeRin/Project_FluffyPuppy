65526ff3a88c0c8e3f84572099dd798ad120d14d
devHyeRin <hyerinlee0v0@gmail.com>
2026-02-03 17:03:41 +0900

    style: 상품 상세 페이지 html/css/js 파일 분리 및 코드 구조화

---
+++ src/main/resources/static/js/item/itemDetail.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+    calculateTotalPrice();
+
+    $("#count").on("change", function(){
+        calculateTotalPrice();
+    });
+
+    formatAllPrices();
+});
+
+/* 총 금액 계산 */
+function calculateTotalPrice(){
+    const count = $("#count").val();
+    const price = $("#price").val();
+    const totalPrice = price * count;
+    $("#totalPrice").html(formatPriceWithCommas(totalPrice) + '원');
+}
+
+/* 장바구니 담기 (AJAX) */
+function addCart(){
+    const token = $("meta[name='_csrf']").attr("content");
+    const header = $("meta[name='_csrf_header']").attr("content");
+
+    const url = "/cart";
+    const paramData = {
+        itemId : $("#itemId").val(),
+        count : $("#count").val()
+    };
+
+    $.ajax({
+        url : url,
+        type : "POST",
+        contentType : "application/json",
+        data : JSON.stringify(paramData),
+        beforeSend : function (xhr){
+            xhr.setRequestHeader(header, token);
+        },
+        dataType : "json",
+        success : function(result){
+            if(confirm("상품을 장바구니에 담았습니다. 장바구니로 이동하시겠습니까?")){
+                location.href='/cart';
+            } else {
+                location.href='/';
+            }
+        },
+        error : function(jqXHR){
+            if(jqXHR.status == '401'){
+                alert('로그인 후 이용해주세요.');
+                location.href='/members/login';
+            } else {
+                // 서버에서 보낸 에러 메시지 출력
+                alert(jqXHR.responseText || "에러가 발생했습니다.");
+            }
+        }
+    });
+}
+
+/* 스크롤 이동 함수 */
+function scrollToSection(sectionId) {
+    $('html, body').animate({
+        scrollTop: $('#' + sectionId).offset().top - 100
+    }, 500);
+}
+
+/* 가격 포맷팅 */
+function formatPriceWithCommas(price) {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function formatAllPrices() {
+    $(".item-price").each(function() {
+        const price = parseInt($(this).text().replace(/[^\d]/g, ""));
+        if(!isNaN(price)) {
+            $(this).text(formatPriceWithCommas(price) + '원');
+        }
+    });
+}
