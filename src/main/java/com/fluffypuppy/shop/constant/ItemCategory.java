d85bc6844c75b9650685ee593b07fafb8edae5af
devHyeRin <hyerinlee0v0@gmail.com>
2026-02-03 15:38:31 +0900

    refactor: 화면 출력을 위한 Enum 클래스 설명 í필드 추가

--- src/main/java/com/fluffypuppy/shop/constant/ItemCategory.java
+++ src/main/java/com/fluffypuppy/shop/constant/ItemCategory.java
@@ -1,6 +1,18 @@
 package com.fluffypuppy.shop.constant;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ItemCategory {
 
-    FOOD, SNACK, BATH, CLOTHES, STUFF, BOWEL
+    FOOD("사료"),
+    SNACK("간식"),
+    BATH("목욕"),
+    CLOTHES("의류"),
+    STUFF("용품"),
+    BOWEL("배변");
+
+    private final String description;
 }
--- src/main/java/com/fluffypuppy/shop/constant/ItemSellStatus.java
+++ src/main/java/com/fluffypuppy/shop/constant/ItemSellStatus.java
@@ -1,7 +1,15 @@
 package com.fluffypuppy.shop.constant;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ItemSellStatus {
-    SELL, SOLD_OUT;
+    SELL("판매중"),
+    SOLD_OUT("품절");
+
+    private final String description;
 
     public boolean isSell(){
         return this == SELL;
