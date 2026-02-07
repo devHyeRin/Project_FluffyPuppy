061d2d6363d862c411903b3dbf5eb019b5bb3caf
devHyeRin <hyerinlee0v0@gmail.com>
2026-02-08 00:03:34 +0900

    로그인 시 에러 메시지 ui 및 js 수정

--- src/main/resources/static/js/member/login.js
+++ src/main/resources/static/js/member/login.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.getElementById('loginForm');
     const signupBtn = document.getElementById('signupBtn');
+    const errorMessage = document.querySelector('.error-message');
 
     signupBtn.addEventListener('click', () => {
         window.location.href = '/members/new';
@@ -10,9 +11,23 @@ document.addEventListener('DOMContentLoaded', () => {
         const email = document.getElementById('email').value.trim();
         const password = document.getElementById('password').value.trim();
 
-        if (!email || !password) {
+        // 기존 에러 숨김
+        errorMessage.classList.add('hidden');
+
+        let message = '';
+
+        if (!email && !password) {
+            message = '이메일과 비밀번호를 입력해주세요.';
+        } else if (!email) {
+            message = '이메일을 입력해주세요.';
+        } else if (!password) {
+            message = '비밀번호를 입력해주세요.';
+        }
+
+        if (message) {
             e.preventDefault();
-            alert('이메일과 비밀번호를 모두 입력해주세요.');
+            errorMessage.textContent = message;
+            errorMessage.classList.remove('hidden');
         }
     });
 });
