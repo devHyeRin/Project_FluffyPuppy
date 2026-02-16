dd8d14de4745e9ca7540b62a500a2a1350fa4a2c
devHyeRin <hyerinlee0v0@gmail.com>
2026-02-16 10:41:17 +0900

    상품 상태 로직 수정 및 ui 개선

--- src/main/java/com/fluffypuppy/shop/entity/Item.java
+++ src/main/java/com/fluffypuppy/shop/entity/Item.java
@@ -67,10 +67,18 @@ public class Item extends BaseEntity{
             throw new OutOfStockException("상품의 재고가 부족합니다.(현재 재고 수량 : " + this.stockNumber + ")");
         }
         this.stockNumber = restStock;
+
+        if (this.stockNumber == 0) {
+            this.itemSellStatus = ItemSellStatus.SOLD_OUT;
+        }
     }
 
     public void addStock(int stockNumber){
         this.stockNumber += stockNumber;
+
+        if (this.stockNumber > 0) {
+            this.itemSellStatus = ItemSellStatus.SELL;
+        }
     }
 }
 
