adabe3dc1181f431ef264adb08d8b58b238b4571
devHyeRin <hyerinlee0v0@gmail.com>
2026-02-26 23:39:18 +0900

    테스트 코드 빌드 오류 해결

--- src/test/java/com/fluffypuppy/shop/FluffypuppyApplicationTests.java
+++ src/test/java/com/fluffypuppy/shop/FluffypuppyApplicationTests.java
@@ -1,13 +1,8 @@
 package com.fluffypuppy.shop;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FluffypuppyApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
